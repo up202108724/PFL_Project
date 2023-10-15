4dae7fc14a267297590ae8b0e9050a563a130162
up202108738 <up202108738@up.pt>
2023-10-15 20:00:28 +0100

    Still Working on the menu

--- menu.pl
+++ menu.pl
@@ -27,13 +27,41 @@ choose_player(Player):-
     get_option(1, 2, 'Select', Index),
     nth1(Index, [player1, player2], Player).
 
+% header/0
+% Game header
+header:-
+    write('===============================================\n'),
+    write('                                             \n'),
+    write('    ( M )( O )( M )( E )( N )( T )( U )( M )        \n'),
+    write('                                            \n'),
+    write('==============================================\n').
+
+% menu/0
+% Main menu
 menu:-  
     write('Game modes:\n'),
     write('1 - Human vs. Human\n'),
     write('2 - Human vs. Computer\n'),
     write('3 - Computer vs. Computer\n').
 
+% set_mode/0
+% Set the game mode
 set_mode :-
     menu,
     get_option(1, 3, 'Select a mode', Option), !,
     option(Option).
+
+
+% get_option(+Min, +Max, +Context, -Value)
+% Get a valid option from the user
+get_option(Min, Max, Context, Value):-
+    format('~a between ~d and ~d: ', [Context, Min, Max]),
+    read_number(Value),
+    (between(Min, Max, Value) -> true ; get_option(Min, Max, Context, Value)).
+
+
+% read_number(-Number)
+% Read a number from the user
+read_number(Number) :-
+    read(Input),
+    (number(Input) -> Number = Input ; write('Invalid input. Please enter a number: '), read_number(Number)).
