028bccfa3085c753a33ead1a1e56dc35a45faec5
unknown <up202108724@g.uporto.pt>
2023-10-31 13:13:23 +0000

    Not Safe

---
+++ configurations.pl
@@ -0,0 +1,78 @@
+:-use_module(library(lists)).
+:-use_module(library(between)).
+
+
+option(1):-
+    write('Human vs. Human\n'),
+    get_name(player1), get_name(player2).
+option(2):-
+    write('Human vs. Computer\n'),
+    get_name(player1),
+    asserta((name_of(player2, 'bot'))), !.
+option(3):-
+    write('Computer vs. Computer\n'),
+    asserta((name_of(player1, 'bot1'))),
+    asserta((name_of(player2, 'bot2'))), !.
+
+% choose_player(-Player)
+% Choose the player who will start the game
+choose_player(Player):-
+    name_of(player1, Name1),
+    name_of(player2, Name2),
+    format('Who starts playing?\n1 - ~a blue\n2 - ~a red\n', [Name1, Name2]),
+    get_option(1, 2, 'Select', Index),
+    nth1(Index, [player1, player2], Player).
+
+% header/0
+% Game header
+header:-
+    write('===============================================\n'),
+    write('                                             \n'),
+    write('    ( M )( O )( M )( E )( N )( T )( U )( M )        \n'),
+    write('                                            \n'),
+    write('==============================================\n').
+
+% menu/0
+% Main menu
+menu:-
+    write('Game modes:\n'),
+    write('1 - Human vs. Human\n'),
+    write('2 - Human vs. Computer\n'),
+    write('3 - Computer vs. Computer\n').
+
+% set_mode/0
+% Set the game mode
+set_mode :-
+    menu,
+    get_option(1, 3, 'Select a mode', Option), !,
+    option(Option).
+
+
+% get_option(+Min, +Max, +Context, -Value)
+% Get a valid option from the user
+get_option(Min, Max, Context, Value):-
+    format('~a between ~d and ~d: ', [Context, Min, Max]),
+    read_number(Value),
+    (between(Min, Max, Value) -> true ; get_option(Min, Max, Context, Value)).
+% read_number(-Number)
+% Read a number from the user
+read_number(Number) :-
+    read(Input),
+    (number(Input) -> Number = Input ; write('Invalid input. Please enter a number: '), read_number(Number)).
+
+% get_name(-Player) 
+% Get the name of a player 
+get_name(Player) :-
+    write('Enter the name: '),
+    read(Name),
+    (is_bot(Name) ->
+        write('The name is not valid. Please choose a different name.\n'),
+        get_name(Player)
+    ; 
+        asserta(name_of(Player, Name))
+    ).
+% is_bot(+Player)
+% Checks if the player is a bot
+
+is_bot(Player) :-
+    member(Player, ['bot', 'bot1', 'bot2']).       
--- menu.pl
+++ menu.pl
@@ -1,114 +1,40 @@
 :- use_module(library(lists)).
-:- use_module(library(system), [now/1]).
 :- consult(dynamicfunctions).
 :- consult(board).
 :- consult(drawerfunctions).
+:- consult(configurations).
 % option(+N)
 % Game mode options.
-option(1):-
-    write('Human vs. Human\n'),
-    get_name(player1), get_name(player2).
-option(2):-
-    write('Human vs. Computer\n'),
-    get_name(player1),
-    asserta((name_of(player2, 'bot'))), !.
-option(3):-
-    write('Computer vs. Computer\n'),
-    asserta((name_of(player1, 'bot1'))),
-    asserta((name_of(player2, 'bot2'))), !.
 
-% choose_player(-Player)
-% Choose the player who will start the game
-choose_player(Player):-
-    name_of(player1, Name1),
-    name_of(player2, Name2),
-    format('Who starts playing?\n1 - ~a blue\n2 - ~a red\n', [Name1, Name2]),
-    get_option(1, 2, 'Select', Index),
-    nth1(Index, [player1, player2], Player).
-
-% header/0
-% Game header
-header:-
-    write('===============================================\n'),
-    write('                                             \n'),
-    write('    ( M )( O )( M )( E )( N )( T )( U )( M )        \n'),
-    write('                                            \n'),
-    write('==============================================\n').
-
-% menu/0
-% Main menu
-menu:-
-    write('Game modes:\n'),
-    write('1 - Human vs. Human\n'),
-    write('2 - Human vs. Computer\n'),
-    write('3 - Computer vs. Computer\n').
-
-% set_mode/0
-% Set the game mode
-set_mode :-
-    menu,
-    get_option(1, 3, 'Select a mode', Option), !,
-    option(Option).
-
-
-% get_option(+Min, +Max, +Context, -Value)
-% Get a valid option from the user
-get_option(Min, Max, Context, Value):-
-    format('~a between ~d and ~d: ', [Context, Min, Max]),
-    read_number(Value),
-    (between(Min, Max, Value) -> true ; get_option(Min, Max, Context, Value)).
-
-between(X, Y, Z) :-
-    X =< Z,
-    Z =< Y.
-
-
-% read_number(-Number)
-% Read a number from the user
-read_number(Number) :-
-    read(Input),
-    (number(Input) -> Number = Input ; write('Invalid input. Please enter a number: '), read_number(Number)).
-
-% get_name(-Player) 
-% Get the name of a player 
-get_name(Player) :-
-    write('Enter the name: '),
-    read(Name),
-    (is_bot(Name) ->
-        write('The name is not valid. Please choose a different name.\n'),
-        get_name(Player)
-    ; 
-        asserta(name_of(Player, Name))
-    ).
-       
 % game_configurations(-GameState) 
 % Set the game configurations 
 game_configurations([Board,Player,[],0]):-     
-       set_mode,      
-       choose_player(Player),     
        header,
+       set_mode,
        init_empty_board(7,Board),
-       initialize_marbles([]).
+       initialize_marbles([]),
+       choose_player(Player).
     
 
 % game_cycle(-GameState)
 % Recursive loop while the game is not over
 
 game_cycle(GameState):-
-    update_board(GameState),
-    (move(GameState, NewGameState) -> 
-        game_cycle(NewGameState)
-    ;   clear_data
-    ).
+    [_, Player,MarblesOnBoard, _] = GameState,
+    is_terminal_state(Player,MarblesOnBoard),!,
+    clear_data.
+game_cycle(GameState):-
+    move(GameState, NewGameState),
+    [Board, _,MarblesOnBoard,TotalMoves] = NewGameState,
+    update_board([Board,_,MarblesOnBoard,_]),
+    change_player(Player,NewPlayer).
+    NewGameState = [Board, NewPlayer,MarblesOnBoard, TotalMoves].
+    game_cycle(NewGameState).
+    
 
 % move(GameState, NewGameState)
 % Game action that builds a new GameState, representing a new move on the game 
 
-move(GameState, NewGameState) :-
-    [Board, Player,MarblesOnBoard, TotalMoves] = GameState,
-    NewGameState=[Board, Player,MarblesOnBoard, TotalMoves],
-    is_terminal_state(Player,MarblesOnBoard), !,
-    fail.
 move(GameState, NewGameState) :-
     [Board, Player,MarblesOnBoard, TotalMoves] = GameState,
      (is_bot(Player) ->  
@@ -119,8 +45,7 @@ move(GameState, NewGameState) :-
         NewTotalMoves is TotalMoves + 1
     ),
     marbles_on_board(MarblesOnBoard),
-    change_player(Player,NewPlayer),
-    NewGameState = [Board, NewPlayer,MarblesOnBoard, NewTotalMoves].
+    NewGameState = [Board,_,MarblesOnBoard, NewTotalMoves].
 
 % play
 % Starts the game and clears data when it ends
@@ -133,7 +58,7 @@ play:-
 % Choose a position to place a marble
 
 choose_position(Player):-
-    format('Enter the row (1-7) and column (1-7) to place a marble (e.g., "3-4"): '),
+    write('Enter the row (1-7) and column (1-7) to place a marble (e.g., "3-4"): '),
     read(Position),
     (valid_position(Position, Player) ->
         true
@@ -151,11 +76,7 @@ valid_position(Position, Player):-
     number_string(Col, ColStr),
     place_marble(Player, Row, Col).
 
-% is_bot(+Player)
-% Checks if the player is a bot
 
-is_bot(Player) :-
-    member(Player, ['bot', 'bot1', 'bot2']).
 
     
 
